ae3e9a716adade4c84030233f37da12845af72fb
Cam Race <cameron.race@education.gov.uk>
2022-03-15 09:50:00 +0000

    Update caveat text

--- R/caveats.R
+++ R/caveats.R
@@ -5,7 +5,7 @@ caveats_box <- function() {
     br(),
     "\U2022 Data only includes graduates who were in sustained employment in the associated tax year, and a graduate's industry is recorded as the industry in which they earnt the most in the associated tax year.",
     br(),
-    "\U2022 Counts have been rounded to the nearest 5, however percentages are based on raw values.",
+    "\U2022 Counts have been rounded to the nearest 5 and earnings are rounded to the nearest £100.",
     br(),
     "\U2022 SIC codes tell us the industry of the company that the graduate works for, and does NOT tell us about the graduates occupation within that company.",
     br()
