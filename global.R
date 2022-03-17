0ce417eb64077cb7eb1631e81eb1ed0728b7027c
FOSTER <Charlotte.FOSTER@education.gov.uk>
2022-03-17 13:25:25 +0000

    ethnicicty and sex nested tables

--- global.R
+++ global.R
@@ -1155,6 +1155,9 @@ crosstabs <- function(subjectinput, YAGinput, countinput, qualinput, buttoninput
   tables_data$SECTIONNAME[is.na(tables_data$SECTIONNAME) == TRUE] <- 'NOT KNOWN'
   tables_earnings_data$SECTIONNAME[is.na(tables_earnings_data$SECTIONNAME) == TRUE] <- 'NOT KNOWN'
   
+  tables_data$group_name[is.na(tables_data$group_name) == TRUE] <- 'NOT KNOWN'
+  tables_earnings_data$group_name[is.na(tables_earnings_data$group_name) == TRUE] <- 'NOT KNOWN'
+  
   orange_pal <- function(x){
     if (!is.na(x)){
       rgb(colorRamp(c("#F7FBFF", "#2F75B5"))(x), maxColorValue = 255)
@@ -1205,9 +1208,11 @@ crosstabs <- function(subjectinput, YAGinput, countinput, qualinput, buttoninput
       mutate_at(c('White', 'Black','Asian', 'Mixed', 'Other', 'Not known'),
                 funs(as.numeric(.))) %>%
       mutate_all(funs(ifelse(!is.na(as.numeric(.)), round(as.numeric(.), -2), .))) %>%
-      select(SECTIONNAME, group_name, White, Black, Asian, Mixed, Other, `Not known`)
+      select(SECTIONNAME, group_name, White, Black, Asian, Mixed, Other, `Not known`) %>%
+      ungroup()
     
-    order <- subset(crosstabs_data, select = SECTIONNAME)
+    
+    order <- subset(crosstabs_data_table, select = SECTIONNAME)
     crosstabs_earnings_data2 <- order %>%
       left_join(crosstabs_earnings_data)
     
@@ -1222,7 +1227,7 @@ crosstabs <- function(subjectinput, YAGinput, countinput, qualinput, buttoninput
     
     footer_data <- tables_data %>%
       filter(sex == 'F+M', subject_name == subjectinput, YAG == YAGinput, FSM == 'All', current_region == 'All', 
-             prior_attainment == 'All', qualification_TR == 'First degree', threshold == thresholdinput) %>%
+             prior_attainment == 'All', qualification_TR == 'First degree', threshold == thresholdinput, group_name == 'All') %>%
       group_by(ethnicity, SECTIONNAME, group_name) %>%
       summarise(n = sum(count)) %>%
       spread(ethnicity, n) %>%
@@ -1231,6 +1236,120 @@ crosstabs <- function(subjectinput, YAGinput, countinput, qualinput, buttoninput
       mutate_all(funs(ifelse( . <= 2, 0, .))) %>%
       select(SECTIONNAME, group_name, White, Black, Asian, Mixed, Other, `Not known`)
     
+    max <- crosstabs_data %>%
+      ungroup() %>%
+      select(-c(group_name, SECTIONNAME))
+    
+    numeric_cols <- names(max)
+    
+    numeric_cols_def <- list()
+    numeric_cols_def_nested <- list()
+    
+    for (column in numeric_cols){
+      script = paste("
+          // source: https://glin.github.io/reactable/articles/examples.html#grouped-cell-rendering-1
+          function(rowInfo) {
+            // source: https://stackoverflow.com/a/44134328/4856719
+            function hslToHex(h, s, l) {
+              l /= 100;
+              const a = s * Math.min(l, 1 - l) / 100;
+              const f = n => {
+                const k = (n + h / 30) % 12;
+                const color = l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
+                return Math.round(255 * color).toString(16).padStart(2, '0');
+              };
+              return `#${f(0)}${f(8)}${f(4)}`;
+            }
+            var value = rowInfo.row['", column, "']
+            var max = ", max(max, na.rm = TRUE), "
+            var min = ", min(max, na.rm = TRUE), "
+            // pct_value = (value - min) * 100 / (max - min)
+            pct_value = (Math.min(value, max) - min) * 100 / (max - min)
+            // If value equals 0, set font color grey.
+            if (value == 0) {
+              var color = '#F7FBFF'
+              var bg = '#F7FBFF'
+            } else {
+              var color = '#000000'
+              var bg = hslToHex(209, 59, 100 - pct_value / 2)
+            }
+            return { color: color, backgroundColor: bg}
+        }", sep="")
+      
+      numeric_cols_def_nested[column] <- list(colDef(
+        na = 'x', style = JS(script), format = colformat,
+      ))
+      
+      numeric_cols_def[column] <- list(colDef(
+        na = 'x', style = JS(script), format = colformat,
+        footer = format(round_any(sum(footer_data[column]),5),big.mark = ",", scientific = FALSE, na.m = T)
+      ))
+      
+    }
+    
+    nested <- function(index){
+      
+      tables_data_nested <- tables_data %>%
+        filter(sex == 'F+M', subject_name == subjectinput, YAG == YAGinput, FSM == 'All', current_region == 'All', 
+               prior_attainment == 'All', qualification_TR == 'First degree', threshold == thresholdinput, group_name != 'All') %>%
+        group_by(ethnicity) %>%
+        mutate(prop = count/sum(count, na.rm = TRUE))
+      
+      nested_table <- tables_data_nested[tables_data_nested$SECTIONNAME == crosstabs_data$SECTIONNAME[index], ] %>%
+        filter(sex == 'F+M', subject_name == subjectinput, YAG == YAGinput, FSM == 'All', current_region == 'All', 
+               prior_attainment == 'All', qualification_TR == 'First degree', threshold == thresholdinput, group_name != 'All') %>%
+        group_by(ethnicity, SECTIONNAME, group_name) %>%
+        summarise(n = prop) %>%
+        spread(ethnicity, n) %>%
+        mutate_all(funs(ifelse(is.na(.), 0, .))) %>%
+        mutate_all(funs(ifelse(. == 0, NA, .)))
+      
+      if('All' %in% names(nested_table)){
+        nested_table <- nested_table %>%
+          arrange(-All)}
+      
+      nested_table_earnings <- tables_data[tables_data$SECTIONNAME == crosstabs_data$SECTIONNAME[index], ] %>%
+        filter(sex == 'F+M', subject_name == subjectinput, YAG == YAGinput, FSM == 'All', current_region == 'All', 
+               prior_attainment == 'All', qualification_TR == 'First degree', threshold == thresholdinput, group_name != 'All') %>%
+        group_by(ethnicity, SECTIONNAME, group_name) %>%
+        summarise(n = earnings_median) %>%
+        spread(ethnicity, n) %>%
+        mutate_all(funs(ifelse(is.na(.), 0, .))) %>%
+        mutate_all(funs(ifelse(. == 0, NA, .))) #%>%
+      # mutate_at(c('White', 'Black','Asian', 'Mixed', 'Other', 'Not known'),
+      #           funs(as.numeric(.))) %>%
+      # mutate_all(funs(ifelse(!is.na(as.numeric(.)), round(as.numeric(.), -2), .))) %>%
+      # select(SECTIONNAME, group_name, White, Black, Asian, Mixed, Other, `Not known`) %>%
+      #ungroup()
+      
+      
+      nested_order <- subset(nested_table, select = c(SECTIONNAME, group_name))
+      nested_order$SECTIONNAME <- as.character(nested_order$SECTIONNAME)
+      nested_order$group_name <- as.character(nested_order$group_name)
+      nested_table_earnings2 <- nested_order %>%
+        left_join(nested_table_earnings)
+      
+      if(buttoninput == 'Proportions'){
+      nested <- nested_table
+      } else if(buttoninput == 'Median earnings'){
+        nested <- nested_table_earnings2
+      }
+      
+      for (column in numeric_cols){
+        nested[column] <- if(column %in% colnames(nested)){nested[column]} else NA
+      }
+      
+      nested <- nested %>%
+        select(SECTIONNAME, group_name, White, Black, Asian, Mixed, Other, `Not known`)
+      
+      htmltools::div(style = "padding: 16px",
+                     reactable(nested, outlined = TRUE, 
+                               style = JS(script), columns = c(coldefs, numeric_cols_def_nested),
+                               defaultPageSize = 300))
+      
+      
+      
+    }
    
   }
 
@@ -1492,17 +1611,20 @@ crosstabs <- function(subjectinput, YAGinput, countinput, qualinput, buttoninput
     
     nested <- function(index){
       
-      nested_table <- tables_data[tables_data$SECTIONNAME == crosstabs_data$SECTIONNAME[index], ] %>%
+      tables_data_nested <- tables_data %>%
+        filter(subject_name == subjectinput, YAG == YAGinput, ethnicity == 'All', current_region == 'All', FSM == 'All', 
+               prior_attainment == 'All', qualification_TR == qualinput, threshold == thresholdinput, group_name != 'All')  %>%
+        group_by(sex) %>%
+        mutate(prop = count/sum(count, na.rm = TRUE))
+      
+      nested_table <- tables_data_nested[tables_data_nested$SECTIONNAME == crosstabs_data$SECTIONNAME[index], ] %>%
         filter(subject_name == subjectinput, YAG == YAGinput, ethnicity == 'All', current_region == 'All', FSM == 'All', 
                prior_attainment == 'All', qualification_TR == qualinput, threshold == thresholdinput, group_name != 'All') %>%
         group_by(sex, SECTIONNAME, group_name) %>%
-        summarise(n = sum(count)) %>%
+        summarise(n = prop) %>%
         spread(sex, n) %>%
         arrange(-`F+M`) %>%
         mutate_all(funs(ifelse(is.na(.), 0, .))) %>%
-        mutate_all(funs(ifelse( . <= 2, 0, .))) %>%
-        ungroup()%>%
-        mutate_if(is.numeric, funs(./sum(.))) %>%
         mutate_all(funs(ifelse(. == 0, NA, .))) %>%
         mutate_at(c('F', 'M', 'F+M'),
                   funs(as.numeric(.))) %>%
