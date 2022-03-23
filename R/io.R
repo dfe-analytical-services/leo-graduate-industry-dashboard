cf6c9d0e23dcf968b67c00ecada182818a962ad9
BIELBY <Richard.BIELBY@education.gov.uk>
2022-03-23 14:43:31 +0000

    Changed my mind and shifted functions into io.R to keep everything together.

--- R/io.R
+++ R/io.R
@@ -1,4 +1,25 @@
 
+read_cohort <- function(cohortfile='data/pg_sankey_data_1_3_yag_dummy.csv'){
+  cohort <- read.csv(cohortfile)
+  cohort <- subset(cohort, select = -X)
+  cohort$SECTIONNAME.x <- StrCap(tolower(cohort$SECTIONNAME.x))
+  cohort$SECTIONNAME.y <- StrCap(tolower(cohort$SECTIONNAME.y))
+  return(cohort)
+}
+
+read_tables_data <- function(file){
+  tables_data <- fread(file) %>% select(-V1)
+  names(tables_data) <- c("X", "YAG", "subject_name", "SECTIONNAME", "sex", 
+                          "ethnicity", "current_region", "FSM", "prior_attainment",
+                          "count", "earnings_median", "threshold", "qualification_TR", 
+                          "group_name")
+  tables_data$SECTIONNAME[tables_data$group_name == "Radio broadcasting"] <- "INFORMATION AND COMMUNICATION"
+  tables_data$SECTIONNAME[tables_data$group_name == "Reproduction of recorded media"] <- "MANUFACTURING"
+  tables_data$SECTIONNAME <- StrCap(tolower(tables_data$SECTIONNAME))
+  return(tables_data)
+}
+
+
 cohort1 <- read_cohort('data/pg_sankey_data_1_3_yag_dummy.csv')
 cohort2 <- read_cohort('data/pg_sankey_data_3_5_yag_dummy.csv')
 cohort3 <- read_cohort('data/pg_sankey_data_1_5_yag_dummy.csv')
