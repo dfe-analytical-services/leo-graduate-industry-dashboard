0f088bd7384d249ce28f295b5f6a691e30fca214
BIELBY <Richard.BIELBY@education.gov.uk>
2022-03-23 15:40:28 +0000

    Removed duplicated read lines.

--- R/read_data.R
+++ R/read_data.R
@@ -27,8 +27,3 @@ read_tables_data <- function(file) {
 }
 
 
-cohort1 <- read_cohort("data/pg_sankey_data_1_3_yag_dummy.csv")
-cohort2 <- read_cohort("data/pg_sankey_data_3_5_yag_dummy.csv")
-cohort3 <- read_cohort("data/pg_sankey_data_1_5_yag_dummy.csv")
-
-table_data <- read_tables_data("data/pg_sic_crosstabs_earnings_data_cf_dummy.csv")
