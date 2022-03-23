ef8b0033adbd7446b9d101b30eff9314b11d3a0f
BIELBY <Richard.BIELBY@education.gov.uk>
2022-03-23 14:42:36 +0000

    Added in contained functions in global.R for io.R to call..

--- R/io.R
+++ R/io.R
@@ -1,27 +1,6 @@
-cohort1 <- read.csv("data/pg_sankey_data_1_3_yag_dummy.csv")
-cohort2 <- read.csv("data/pg_sankey_data_3_5_yag_dummy.csv")
-cohort3 <- read.csv("data/pg_sankey_data_1_5_yag_dummy.csv")
 
-cohort1 <- subset(cohort1, select = -X)
-cohort2 <- subset(cohort2, select = -X)
-cohort3 <- subset(cohort3, select = -X)
+cohort1 <- read_cohort('data/pg_sankey_data_1_3_yag_dummy.csv')
+cohort2 <- read_cohort('data/pg_sankey_data_3_5_yag_dummy.csv')
+cohort3 <- read_cohort('data/pg_sankey_data_1_5_yag_dummy.csv')
 
-cohort1$SECTIONNAME.x <- StrCap(tolower(cohort1$SECTIONNAME.x))
-cohort1$SECTIONNAME.y <- StrCap(tolower(cohort1$SECTIONNAME.y))
-cohort2$SECTIONNAME.x <- StrCap(tolower(cohort2$SECTIONNAME.x))
-cohort2$SECTIONNAME.y <- StrCap(tolower(cohort2$SECTIONNAME.y))
-cohort3$SECTIONNAME.x <- StrCap(tolower(cohort3$SECTIONNAME.x))
-cohort3$SECTIONNAME.y <- StrCap(tolower(cohort3$SECTIONNAME.y))
-
-tables_data <- fread("data/pg_sic_crosstabs_earnings_data_cf_dummy.csv") %>% select(-V1)
-
-names(tables_data) <- c("X", "YAG", "subject_name", "SECTIONNAME", "sex", "ethnicity", "current_region", "FSM", "prior_attainment", "count", "earnings_median", "threshold", "qualification_TR", "group_name")
-
-tables_data$SECTIONNAME[tables_data$group_name == "Radio broadcasting"] <- "INFORMATION AND COMMUNICATION"
-tables_data$SECTIONNAME[tables_data$group_name == "Reproduction of recorded media"] <- "MANUFACTURING"
-
-tables_data$SECTIONNAME <- StrCap(tolower(tables_data$SECTIONNAME))
-
-tables_earnings_data <- tables_data
-
-names(tables_earnings_data) <- c("X", "YAG", "subject_name", "SECTIONNAME", "sex", "ethnicity", "current_region", "FSM", "prior_attainment", "count", "earnings_median", "threshold", "qualification_TR", "group_name")
+table_data <- read_tables_data("data/pg_sic_crosstabs_earnings_data_cf_dummy.csv")
--- global.R
+++ global.R
@@ -48,3 +48,24 @@ tidy_code_function <- function() {
   script_changes <- c(app_scripts, r_scripts, test_scripts)
   return(script_changes)
 }
+
+
+read_cohort <- function(cohortfile='data/pg_sankey_data_1_3_yag_dummy.csv'){
+  cohort <- read.csv(cohortfile)
+  cohort <- subset(cohort, select = -X)
+  cohort$SECTIONNAME.x <- StrCap(tolower(cohort$SECTIONNAME.x))
+  cohort$SECTIONNAME.y <- StrCap(tolower(cohort$SECTIONNAME.y))
+  return(cohort)
+}
+
+read_tables_data <- function(file){
+  tables_data <- fread(file) %>% select(-V1)
+  names(tables_data) <- c("X", "YAG", "subject_name", "SECTIONNAME", "sex", 
+                          "ethnicity", "current_region", "FSM", "prior_attainment",
+                          "count", "earnings_median", "threshold", "qualification_TR", 
+                          "group_name")
+  tables_data$SECTIONNAME[tables_data$group_name == "Radio broadcasting"] <- "INFORMATION AND COMMUNICATION"
+  tables_data$SECTIONNAME[tables_data$group_name == "Reproduction of recorded media"] <- "MANUFACTURING"
+  tables_data$SECTIONNAME <- StrCap(tolower(tables_data$SECTIONNAME))
+  return(tables_data)
+}
