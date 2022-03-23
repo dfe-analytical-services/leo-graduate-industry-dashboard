f045226aae1180b0fdc4750c855659162d0b970d
BIELBY <Richard.BIELBY@education.gov.uk>
2022-03-23 15:07:33 +0000

    Giving GH Actions a nudge.

--- R/io.R
+++ R/io.R
@@ -1,4 +1,8 @@
+# Filename: io.R
+# Created: 23/03/22 
+# Description: Functions for reading in and outputing data from/to files.
 
+# Read in a cohort file.
 read_cohort <- function(cohortfile = "data/pg_sankey_data_1_3_yag_dummy.csv") {
   cohort <- read.csv(cohortfile)
   cohort <- subset(cohort, select = -X)
@@ -7,6 +11,7 @@ read_cohort <- function(cohortfile = "data/pg_sankey_data_1_3_yag_dummy.csv") {
   return(cohort)
 }
 
+# Read in tables data.
 read_tables_data <- function(file) {
   tables_data <- fread(file) %>% select(-V1)
   names(tables_data) <- c(
