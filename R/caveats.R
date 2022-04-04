94c0b68208765820edb20f3f90bc3fb278f588cf
Cam Race <cameron.race@education.gov.uk>
2022-04-04 21:07:44 +0100

    add sample accessibility statement

--- R/caveats.R
+++ R/caveats.R
@@ -1,15 +1,16 @@
 caveats_box <- function() {
   insert_text(
-    inputId = "caveats", #status = "warning", width = NULL, solidHeader = TRUE,
+    inputId = "caveats",
     text = paste0(
       strong("Caveats"),
-    br(),
-    br(),
-    "\U2022 Data only includes graduates who were in sustained employment in the associated tax year, and a graduate's industry is recorded as the industry in which they earnt the most in the associated tax year.",
-    br(),
-    "\U2022 Counts have been rounded to the nearest 5 and earnings are rounded to the nearest £100.",
-    br(),
-    "\U2022 SIC codes tell us the industry of the company that the graduate works for, and does NOT tell us about the graduates occupation within that company.",
-    br())
+      br(),
+      br(),
+      "\U2022 Data only includes graduates who were in sustained employment in the associated tax year, and a graduate's industry is recorded as the industry in which they earnt the most in the associated tax year.",
+      br(),
+      "\U2022 Counts have been rounded to the nearest 5 and earnings are rounded to the nearest £100.",
+      br(),
+      "\U2022 SIC codes tell us the industry of the company that the graduate works for, and does NOT tell us about the graduates occupation within that company.",
+      br()
+    )
   )
 }
--- ui.R
+++ ui.R
@@ -230,12 +230,11 @@ fluidPage(
           )
         )
       ),
-      
+
       ## Caveats ---------------------------------------------------------------------
-      
+
       caveats_box(), # defined in R/caveats.R
       br()
-      
     ),
 
     # Regional analysis tab ------------------------------------------
@@ -363,12 +362,11 @@ fluidPage(
           )
         )
       ),
-      
+
       ## Caveats ---------------------------------------------------------------------
-      
+
       caveats_box(), # defined in R/caveats.R
       br()
-      
     ),
 
     # Subject by industry tab ---------------------------------------------
@@ -457,12 +455,11 @@ fluidPage(
           withSpinner(reactableOutput("crosstab"))
         )
       ),
-      
+
       ## Caveats ---------------------------------------------------------------------
-      
+
       caveats_box(), # defined in R/caveats.R
       br(),
-      
     ),
 
     # Industry by subject tab -------------------------------------
@@ -564,12 +561,11 @@ fluidPage(
           withSpinner(reactableOutput("crosstab_backwards"))
         )
       ),
-      
+
       ## Caveats ---------------------------------------------------------------------
-      
+
       caveats_box(), # defined in R/caveats.R
       br(),
-      
     ),
 
     # Support links ----------------------------------------------
@@ -578,22 +574,30 @@ fluidPage(
       "Support and feedback",
       h2("Give us feedback"),
       br(),
-      "This dashboard is a new service that we are developing. If you have any feedback or suggestions for improvements, please submit them using our [feedback survey].",
+      "This dashboard is a new service that we are developing. If you have any feedback or suggestions for improvements, please submit them using our ",
+      a(href = "", "feedback form", .noWS = c("after")),
+      ".",
       br(),
       br(),
       h2("Find more information on the data"),
       br(),
-      "The data in the dashboard, along with methodological information can be found on [Explore Education Statistics].",
+      "The data used to produce the dashboard, along with methodological information can be found on ",
+      a(href = "https://explore-education-statistics.service.gov.uk/", "Explore Education Statistics", .noWS = c("after")),
+      ".",
       br(),
       br(),
       h2("Contact us"),
       br(),
-      "If you have questions about the dashboard, please contact us at [he.leo@education.gov.uk]",
+      "If you have questions about the dashboard or data within it, please contact us at ",
+      a(href = "mailto:he.leo@education.gov.uk", "he.leo@education.gov.uk", .noWS = c("after")),
+      ".",
       br(),
       br(),
       h2("See the source code"),
       br(),
-      "The source code for this dashboard is available in our [GitHub repository]",
+      "The source code for this dashboard is available in our ",
+      a(href = "https://github.com/dfe-analytical-services/leo-graduate-industry-dashboard", "GitHub repository", .noWS = c("after")),
+      ".",
       br(),
       br(),
       br(),
@@ -606,37 +610,76 @@ fluidPage(
 
     tabPanel(
       "Accessibility",
+      
       warning_text(inputId = "accessWarn", text = "THIS IS A DRAFT STATEMENT - NEEDS UPDATING AFTER TESTING"),
-      h2("Accessibility statement"),
-      "This accessibility statement applies to the Local Authority (LA) scorecards application.",
+      
+      h2("Accessibility statement for LEO Graduate Industry dashboard"),
+      br(),
+      "This accessibility statement applies to the Longitudinal Education Outcomes (LEO) graduate industry dashboard.",
       "This application is run by the Department for Education. We want as many people as possible to be able to use this application,
             and have actively developed this application with accessibilty in mind.",
-      h3("WCAG 2.1 compliance"),
+      br(),
+      br(),
       "We follow the reccomendations of the ", a(href = "https://www.w3.org/TR/WCAG21/", "WCAG 2.1 requirements", .noWS = c("after")),
       ". This application has been checked using the ", a(href = "https://github.com/ewenme/shinya11y", "Shinya11y tool", .noWS = c("after")),
-      ", which did not detect accessibility issues.", "This application also fully passes the accessibility audits checked by DfE analysts using the ",
+      ", which did not detect accessibility issues.", # TODO: CHECK THIS!
+      "This application also fully passes the accessibility audits checked by DfE analysts using the ",
       a(href = "https://developers.google.com/web/tools/lighthouse", "Google Developer Lighthouse tool", .noWS = c("after")),
       ". This means that this application:",
+      br(),
       tags$div(tags$ul(
         tags$li("uses colours that have sufficient contrast"),
         tags$li("allows you to zoom in up to 300% without the text spilling off the screen"),
         tags$li("has its performance regularly monitored, with a team working on any feedback to improve accessibility for all users")
-      )),
-      h3("Limitations"),
+      )), # TODO: UPDATE THESE BULLET POINTS
+      "We've also made the website text as simple as possible to understand.",
+      a(href = "https://mcmw.abilitynet.org.uk/", "AbilityNet", .noWS = c("after")),
+      " has advice on making your device easier to use if you have a disability.",
+      
+      h3("How accessible this website is"),
+      br(),
       "We recognise that there are still potential issues with accessibility in this application, but we will continue
            to review updates to technology available to us to keep improving accessibility for all of our users. For example, these
            are known issues that we will continue to monitor and improve:",
       tags$div(tags$ul(
         tags$li("Keyboard navigation through the interactive charts is currently limited"),
         tags$li("Alternative text in interactive charts is limited to titles and could be more descriptive (although this data is available in csv format)")
-      )),
-      h3("Feedback"),
-      "If you have any feedback on how we could further improve the accessibility of this application, please contact us at",
+      )), # TODO: UPDATE THESE BULLET POINTS
+      
+      h3("Feedback and contact information"),
+      br(),
+      "If you need information on this website in a different format like accessible PDF, large print, easy read, audio recording or braille or
+      if you have any feedback on how we could further improve the accessibility of this application, please contact us at ",
       a(href = "mailto:he.leo@education.gov.uk", "he.leo@education.gov.uk", .noWS = c("after")),
       ".",
       br(),
+      br(),
+      
+      h2("Technical information about this website’s accessibility"),
+      br(),
+      "Department for Education is committed to making its website accessible, in accordance with the Public Sector Bodies 
+      (Websites and Mobile Applications) (No. 2) Accessibility Regulations 2018.",
+      
+      h3("Compliance status"),
+      br(),
+      "This website is partially compliant with the ",
+      a(href="", "Web Content Accessibility Guidelines version 2.1", .noWS = c("after")),
+      " AA standard, due to [insert one of the following: ‘the non-compliances’, ‘the exemptions’ or ‘the non-compliances and exemptions’] listed below.",
+      br(),
+      br(),
+      "List of any non-compliances", # TODO: List the non-compliances using the appropriate template
+      
+      h2("Preparation of this accessibility statement"),
+      br(),
+      "This statement was prepared on [date when it was first published].", # TODO: Add date
+      br(),
+      br(),
+      "This website was last tested on [date]. The test was carried out by Department for Education.", # TODO: Add date
+      br(),
+      br(),
       br()
     )
+    
   ), # navbar page
 
   # Footer ----------------------------------------------
